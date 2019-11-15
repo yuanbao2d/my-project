df1be0d89096000e78fe6d9f4e91638c878791c1
yuanbo2d <503114750@qq.com>
2019-11-15 13:56:44 +0800

    common

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+async function asyncAwaitTask() {
+  const { version } = fs.readFileSync('package.json');
+  console.log(version);
+  await Promise.resolve('some result');
+}
+
+exports.default = asyncAwaitTask;
